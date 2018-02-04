912af57d4d9d1171a6a2f29d65deea7d175c529f
alkitch <alan_kitching@hotmail.com>
2018-02-04 13:09:32 +0000

    Initial working version

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -pthread -g prodmain.cpp prodcom.cpp prodnfc.cpp prodweb.cpp -o prodcom -lwiringPi -lnfc -lchilkat-9.5.0 -L/home/pi/chilkat-9.5.0/lib -L/home/pi/libnfc/libnfc -I/home/pi/chilkat-9.5.0/include -I/home/pi/libnfc/include/nfc 
---
+++ prodweb.cpp
@@ -0,0 +1,92 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <wiringPi.h>
+#include <CkJsonObject.h>
+#include <CkHttpRequest.h>
+#include <CkHttp.h>
+#include <CkHttpResponse.h>
+#include <CkXml.h>
+#include "prodcom.h"
+
+
+bool HttpPrepare(int v);
+
+bool web_init(void)
+{
+   CkHttp httx;
+   bool success = httx.UnlockComponent("Anything for 30-day trial");
+    if (success != true) {
+        printf("%s\r\n", httx.lastErrorText() );
+        return false;
+    }
+	return true;
+}
+
+
+
+void* web_out_handler(void *nfc_context) {
+
+    cp_t* temp = (cp_t *)nfc_context;
+    pthread_barrier_wait(&temp->bar);
+    for(; ;) { 
+
+        //acquire the lock
+        pthread_mutex_lock(&temp->lock);
+
+    while(temp->occupied == 0) 
+            pthread_cond_wait(&temp->uid, &temp->lock);
+
+
+    HttpPrepare( temp->nfcuid[temp->nextout] );
+    //increment counters
+    temp->occupied--;
+    temp->nextout++;
+    temp->nextout %= temp->capacity;
+    temp->uids_out++;
+
+
+    //somebody may be waiting on toom to become available
+    pthread_cond_signal(&temp->space);
+
+    //release the locl
+     pthread_mutex_unlock(&temp->lock);
+
+
+    }
+    return ((void *)NULL);
+
+}
+
+bool HttpPrepare(int v)
+{
+    CkHttp http;
+    CkJsonObject json;
+
+    bool success = json.AddIntAt(-1,"RFDidTag",v);
+    if(success)
+    {
+     success = json.AddStringAt(-1,"RFDidSectorData","FrontDoor");
+     json.put_EmitCompact(true);
+    }
+
+    http.put_AcceptCharset("");
+    http.put_UserAgent("");
+    http.put_AcceptLanguage("");
+    http.put_AllowGzip(false);
+    success = http.AddQuickHeader("Content-type","application/json");
+
+     
+	CkHttpResponse *resp = http.PostJson("http://alansw550/a2427/api/values",json.emit() ); 
+    if (resp == 0 ) {
+        printf("%s\r\n", http.lastErrorText() );
+        return false;
+    }
+    else {
+        //  Display the JSON response.
+        printf("Response Body = %s\r\n", resp->bodyStr() );
+        delete resp;
+    }
+    return true;
+}
